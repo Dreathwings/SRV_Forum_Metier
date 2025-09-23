ea8c125d32880986c89029c83e3029af4c333eec
Dreathwings <63842774+Dreathwings@users.noreply.github.com>
2025-09-23 21:08:21 +0200

    Fix SQL insert query string quoting

--- serveur.py
+++ serveur.py
@@ -59,7 +59,7 @@ def validate():
     data = request.form
     DB = connect_to_DB_forum_metier()
     cur = DB.cursor()
-    cur.execute("INSERT INTO `forum-metier`.DATA(ent_nom,ent_adresse,ent_cp,ent_ville,ent_desc,parcours,nb_p,p1_nom,p1_prenom,p1_email,p1_poste,p1_dej,p2_nom,p2_prenom,p2_email,p2_poste,p2_dej,p3_nom,p3_prenom,p3_email,p3_poste,p3_dej) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+    cur.execute("""INSERT INTO `forum-metier`.DATA(ent_nom,ent_adresse,ent_cp,ent_ville,ent_desc,parcours,nb_p,p1_nom,p1_prenom,p1_email,p1_poste,p1_dej,p2_nom,p2_prenom,p2_email,p2_poste,p2_dej,p3_nom,p3_prenom,p3_email,p3_poste,p3_dej) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
                 ,(data.get("entreprise_nom",None),
                   data.get("entreprise_adresse",None),
                   data.get("code_postal",None),
